a22d9de2fb0d8351491a349399e07dbb9dedc4a7
Johnny Abou Haidar <johnnyabuhaydar@gmail.com>
2025-11-13 15:29:56 +0200

    added from batch file

---
+++ commitandpush.bat
@@ -0,0 +1,3 @@
+git add *
+git commit -m "added from batch file"
+git push
---
+++ main.py
@@ -0,0 +1,5 @@
+import json
+from sample_payload import *
+
+vendor_input_OBJ=json.load(input_payload)
+print(vendor_input_OBJ)
